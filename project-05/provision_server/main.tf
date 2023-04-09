30a8daa4b77aed5710319404463f8ea6fa014f5a
Ankit <ankit.raz@outlook.com>
2023-04-09 15:04:04 +0530

    added files

---
+++ project-05/provision_server/main.tf
@@ -0,0 +1,81 @@
+terraform {
+  required_providers {
+    digitalocean = {
+      source = "digitalocean/digitalocean"
+      version = "~> 2.0"
+    }
+  }
+}
+
+variable "do_token" {}
+
+provider "digitalocean" {
+  token = var.do_token
+}
+
+resource "digitalocean_droplet" "jenkins-server" {
+    image = "ubuntu-20-04-x64"
+    name = "ci-server"
+    region = "blr1"
+    size = "s-2vcpu-4gb"
+    ssh_keys = [digitalocean_ssh_key.my-ssh-key.fingerprint]
+    tags = [ "dev" ]
+}
+
+
+resource "digitalocean_ssh_key" "my-ssh-key" {
+    name = "server-key"
+    public_key = file("~/.ssh/id_rsa.pub")
+}
+
+resource "digitalocean_firewall" "web" {
+  name        = "only-22-80-firewall"
+  droplet_ids = [digitalocean_droplet.jenkins-server.id]
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "22"
+    source_addresses = ["0.0.0.0/0"]
+  }
+
+  inbound_rule {
+    protocol         = "tcp"
+    port_range       = "80"
+    source_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+
+  outbound_rule {
+    protocol              = "tcp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "icmp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+
+  outbound_rule {
+    protocol              = "udp"
+    port_range            = "1-65535"
+    destination_addresses = ["0.0.0.0/0", "::/0"]
+  }
+}
+
+output "droplet-id" {
+    value = digitalocean_droplet.jenkins-server.ipv4_address
+}
+
+output "droplet-cost" {
+    value = digitalocean_droplet.jenkins-server.price_monthly
+}
+
+resource "null_resource" "configure_server" {
+    provisioner "local-exec" {
+        command = "ansible-playbook --inventory ${digitalocean_droplet.jenkins-server.ipv4_address}, --user root --private-key ~/.ssh/id_rsa /home/ankit/DevOps-Projects/project-05/configure_server/playbook.yaml"
+    }
+    triggers = {always_run = "${uuid()}"}
+}
+
